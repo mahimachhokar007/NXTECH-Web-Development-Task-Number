fb0e6d8cd324208de90fd0c2872b79af23f00ce6
Mahima Chhokar <140743630+mahimachhokar007@users.noreply.github.com>
2023-09-10 12:54:54 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'tera yaar hu main',
+        path: 'musics/song1.mp3',
+        artist: 'Arijit Singh',
+        cover: 'images/images.jpg'
+    },
+    {
+        name: 'yaaro ne mere vaste',
+        path: 'musics/song2.mp3',
+        artist: 'Rajiv Raja',
+        cover: 'images/image2.jpg'
+    },
+    {
+        name: 'Yaara Yaara',
+        path: 'musics/song3.mp3',
+        artist: 'Udit Narayan, Alka yagnik',
+        cover: 'images/image3.jpg'
+    },
+    {
+        name: 'Yaaro ki yaari',
+        path: 'musics/song4.mp3',
+        artist: 'Unknown',
+        cover: 'images/image4.jpg'
+    },
+    {
+        name: 'Yaara',
+        path: 'musics/song5.mp3',
+        artist: 'Mamta Sharma',
+        cover: 'images/image5.jpg'
+    },
+]
